fe03929781f689a805039dea867f148d0e817923
Alpha57705 <127438241+Alpha57705@users.noreply.github.com>
2023-05-25 12:24:49 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+# make a new variable for user input
+
+var = input('Enter any value: ')
+
+# make a function 
+
+def value_input(): {
+    print(value_input)
+}
+
+# Make another variable to call
+
+value = value_input
+
+# Call the Function through a random input
+
+if 'random' or 'Random' in var: {
+    print(value)
+}
